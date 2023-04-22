e411ac99d80820f3e6c9f44f385b3ed1e6674613
lklklkllkllklklklkl <128761622+Greedplusplus@users.noreply.github.com>
2023-04-22 12:06:48 -0600

    big changes
    i will not go further

--- public/js/command.js
+++ public/js/command.js
@@ -1,87 +1,31 @@
 var portfolio = "https://www.bbkx.live/"
 var linkedin = "https://www.linkedin.com/in/bbkx/"
-var github = "https://github.com/bbkx226"
-var facebook = "https://www.facebook.com/bbkx.0226/"
-var instagram = "https://www.instagram.com/bbkx_/"
-var discord = "https://discord.com/oauth2/authorize?client_id=816611024362668122&permissions=156766824512&scope=bot"
-var aimagenerator = "https://imagegeneratorai-6e92d.web.app/"
-var blockchain = "https://blockchainapp-4e293.web.app/"
-var guzheng = "https://app.engati.com/static/standalone/bot.html?bot_key=63ef367dfa364830&env=p"
-var iserv = "https://www.figma.com/proto/utxhK2W3AYMe2FVdEgIaGw/SAAD-Prototype?node-id=0%3A1&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=10%3A138&show-proto-sidebar=1"
-var aqualert = "https://www.figma.com/proto/XKmlWK1PPpaWHPGONSpZfn/OH-Water-Pollution?node-id=431%3A371&scaling=scale-down&page-id=414%3A2&starting-point-node-id=431%3A371"
-var connectfour = "https://github.com/bbkx226/Games/blob/main/connectfour.py"
-var pong = "https://github.com/bbkx226/Games/blob/main/pong.py"
-var snake = "https://github.com/bbkx226/Games/blob/main/snake.py"
-var tetris = "https://github.com/bbkx226/Games/blob/main/tetris.py"
-var email ="mailto:bbkx226@gmail.com"
+var github = "https://github.com/Greedplusplus"
+var email ="mailto:spencerbaue@gmail.com"
 
 help = [
     '+========================================================================================+',
     '| Commands   | Descriptions                                                              |',
     '+========================================================================================+',
-    '| whois      | Who is Brandon Ban?                                                       |',
-    '| bbkx       | Discover more about me                                                    |',
-    '| projects   | View Coding Projects                                                      |',
-    '| social     | Display Social Networks                                                   |',
+    '| aboutme    | Who is Brandon Ban?                                                       |',
+    '| projects   | dont bother, currently empty                                              |',
     '| history    | View command history                                                      |',
-    '| help       | Ah, yes. You\'re a command whisperer! You already know what this one does. |',
     '| email      | Please don\'t spam my email                                                |',
     '| clear      | Clear terminal                                                            |',
     '| banner     | Display the header                                                        |',
-    '| LinkedIn   | Checkout my LinkedIn profile for the latest updates!                      |',
-    '| GitHub     | Discover more projects on my GitHub profile                               |',
-    '| Facebook   | Contact me via Facebook                                                   |',
-    '| Instagram  | Don\'t expect much on it as I didn\'t post anything xD                      |',
-    '| bmttuxd    | Unveil the obscurity that lies within me.                                 |',
     '+========================================================================================+',
 ]
 
-whois = [
+aboutme = [
     "<br>",
-    "Welcome to my Terminal Website, I'm <span class=\"command\">Brandon</span>!😎",
+    "I am bored my name is also Spencer for those curious",
     "<br>",
-    "- I am currently studying computer science at the Asia Pacific University of Technology and Innovation.",
-    "- Born in Johore, Malaysia.",
-    "<br>",
-    "- As a <span class=\"command\">highly motivated</span> and <span class=\"command\">disciplined</span> computer science student, I am deeply committed to pursuing a career in full stack development.",
-    "<br>",
-    "- Through a combination of <span class=\"command\">self-study</span> and collaboration with my peers,",
-    "  I am continuously working to hone my skills and achieve my goal of becoming a full-fledged <span class=\"command\">full stack developer</span>.",
-    "<br>",
-    "- I am also keen to <span class=\"command\">challenge myself</span> by venturing beyond my comfort zone and embracing new opportunities and challenges.",
-    "<br>",
-    "- One of the things I enjoy about programming is the opportunity to develop something that will <span class=\"command\">make our lives easier and more efficient</span>.",
-    "<br>",
-    "- Apart from that, it allows me to collaborate with people from various disciplines who share my interests.",
-    "<br>",
-    "- It's fantastic to collaborate with other professionals who are striving toward the same goal.",
-    "<br>",
-]
-
-bbkx = [
-    "<br>",
-    'For a comprehensive overview of my qualifications and expertise, I kindly direct you to my <a href="' + portfolio + '" target="_blank">portfolio website.' + "</a>",
-    "<br>"
 ]
 
 projects = [
     "<br>",
-    "<span class=\"command\">Code Projects</span>",
-    '<a href="' + discord + '" target="_blank">Discord Bot' + "</a>",
-    '<p class="terminal">Terminal Website (The one you\'re using right now!)</p>',
-    '<a href="' + aimagenerator + '" target="_blank">AI Image Generator' + "</a>",
-    '<a href="' + blockchain + '" target="_blank">Blockchain Web3 Application' + "</a>",
-    '<a href="' + guzheng + '" target="_blank">Guzheng Chatbot' + "</a>",
-    "<br>",
-    "<span class=\"command\">Figma Prototypes</span>",
-    '<a href="' + iserv + '" target="_blank">Cleaning Service System Prototype' + "</a>",
-    '<a href="' + aqualert + '" target="_blank">Aqua Alert' + "</a>",
-    "<br>",
-    "<span class=\"command\">Mini games</span>",
-    '<a href="' + connectfour + '" target="_blank">Connect Four' + "</a>",
-    '<a href="' + pong + '" target="_blank">Pong' + "</a>",
-    '<a href="' + snake + '" target="_blank">Snake' + "</a>",
-    '<a href="' + tetris + '" target="_blank">Tetris' + "</a>",
+    "I told you it's empty",
+    '<span class=\"command\">GitHub</span>  <a href="' + github + '" target="_blank">GitHub/Greedorahplusplus' + "</a>",
     "<br>",
 ]
 
@@ -91,7 +35,7 @@ social = [
     '<span class=\"command\">GitHub</span>         <a href="' + github + '" target="_blank">GitHub/bbkx226' + "</a>",
     '<span class=\"command\">Facebook</span>       <a href="' + facebook + '" target="_blank">Facebook/Brandon Ban' + "</a>",
     '<span class=\"command\">Instagram</span>      <a href="' + instagram + '" target="_blank">Instagram/Brandon Ban' + "</a>",
-    "<br>"
+    "<br>",
 ]
 
 banner = [
@@ -108,12 +52,6 @@ banner = [
                                                                                                          
 ]
                                                                                                                                   
-suggestion = [
-  "<p class>",
-  "<span class=\"color2\">For a list of available commands, type <span class=\"command\">help</span> .",
-  "</span>",
-  "</p>",
-]
 
 secret = [
   "<br>",
